700cb695b80f015efa6ddc44e051a45af03415bb
jfeldhege <jfeldhege+git@gmail.com>
2019-09-21 13:58:55 +0200

    First commit

---
+++ redditR.r
@@ -0,0 +1,12 @@
+#' redditR: a package to access the Reddit (www.reddit.com) API
+#'
+#' The redditR package provides a number of functions that can access different
+#' endpoints of the Reddit API:
+#'
+#'
+#' @section get_ functions:
+#' The get_ functions access different endpoints
+#'
+#' @docType package
+#' @name redditR
+NULL
